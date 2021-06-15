d90998215410234573df2bf2b7fb14847e9d36b8
PavelGem <pashentsev.1@gmail.com>
2021-06-15 13:01:39 +0300

    add Custom Skybox

--- City-scene-editor/Assets/Scripts/CameraSettingsController.cs
+++ City-scene-editor/Assets/Scripts/CameraSettingsController.cs
@@ -8,23 +8,40 @@ public class CameraSettingsController : MonoBehaviour
     [SerializeField] Camera camera;
 
 
-    [SerializeField] Slider cameraZoom;
-    [SerializeField] Dropdown projections;
+    [SerializeField] Slider cameraZoomSlider;
+    [SerializeField] Dropdown projectionsDropdown;
+    [SerializeField] Dropdown skyboxDropdown;
+
+    [SerializeField] Material defaultMaterial;
+    [SerializeField] Material customMaterial;
+
     public void ChangeCameraZoom()
     {
-        camera.fieldOfView = cameraZoom.value;
-        camera.orthographicSize = cameraZoom.value;
+        camera.fieldOfView = cameraZoomSlider.value;
+        camera.orthographicSize = cameraZoomSlider.value;
     }
 
     public void ChangePerspective() 
     {
-        if (projections.value==0)
+        if (projectionsDropdown.value==0)
         {
             camera.orthographic = false;
         }
         else 
         {
             camera.orthographic = true;
+        }
+    }
+
+    public void ChangeSkyBox() 
+    {
+        if (skyboxDropdown.value == 0)
+        {
+            RenderSettings.skybox = defaultMaterial;
+        }
+        else 
+        {
+            RenderSettings.skybox = customMaterial;
         }
     }
 }
